e4e71ddcf0e5a3926b705e3b2b4aba1995366916
Lauren Byrne <lbyrne7@my.bcit.ca>
2023-01-04 14:39:15 -0800

    added get and post request

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const songs = [
+  {
+    id: 1,
+    title: 'Super Trouper',
+    artist: 'ABBA',
+  },
+];
+
+app.get('/api/songs', (req, res) => {
+  console.log('GET /api/songs');
+  res.send({ songs: songs });
+});
+
+app.post('/api/songs', (req, res) => {
+  const data = req.body;
+  console.log('POST /api/songs', data);
+  data.id = songs.length + 1;
+  songs.push(data);
+  res.send(data);
+});
+
 app.get('/', (req, res) => {
   console.log('GET /');
   res.send('<h1>hello aws</h1>');
